ba211f83cae96577b09f636fedbacbe73190499a
nikolasmelui <slak@samaradom.ru>
2019-02-10 12:18:10 +0400

    Add new argument and return types to the functions

--- app.ts
+++ app.ts
@@ -1,1 +1,32 @@
-console.log('Hello, typed world!');
+// tuples
+const tuplesArray: [string, number] = ['Hello', 100];
+console.log(tuplesArray);
+
+// enums
+const enum Color {
+  Red,
+  Green,
+  Blue
+}
+const myColor: Color = Color.Blue;
+console.log(myColor);
+
+const Nikolay = {
+  name: 'Nikolay',
+  age: 27
+};
+
+// Return the name
+function returnName(): string {
+  return Nikolay.name;
+}
+// Print the name
+function sayName(): void {
+  console.log(Nikolay.name);
+}
+
+// Argument types
+function multiply(value1: number, value2: number): number {
+  return value1 * value2;
+}
+console.log(multiply(1, 2));
