1fbfd0f2894c87f3f49ab87162b0d8cc217a1580
nikolasmelui <slak@samaradom.ru>
2019-02-13 01:25:32 +0400

    Add some simple expamples of necessary props and methods in interfaces

--- interfaces/app.ts
+++ interfaces/app.ts
@@ -1,7 +1,9 @@
 namespace InterfacedPerson {
   interface NamedPerson {
     firstName: string;
-    age: number;
+    age?: number;
+    [propName: string]: any;
+    greet(greetWord: string): void;
   }
 
   const greet = (person: NamedPerson): void =>
@@ -13,10 +15,15 @@ namespace InterfacedPerson {
 
   const person: NamedPerson = {
     firstName: 'Nikolay',
-    age: 27
+    age: 27,
+    hobbies: ['Cooking', 'Sports'],
+    greet(greetWord: string): void {
+      console.log(greetWord);
+    }
   };
 
   greet(person);
   changeName(person, 'Nikolas');
   greet(person);
+  person.greet('Hello');
 }
