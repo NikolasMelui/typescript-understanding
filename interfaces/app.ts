30d719d046ee1736e7aad75ed3ee3ef7de1189d5
nikolasmelui <slak@samaradom.ru>
2019-02-13 01:39:46 +0400

    Add the function type interface using

--- interfaces/app.ts
+++ interfaces/app.ts
@@ -39,4 +39,15 @@ namespace InterfacedPerson {
   changeName(person, 'Nikolas');
   greet(person);
   person.greet('Hello');
+
+  // Function Types Interface
+  interface DoubleValueFunc {
+    (number1: number, number2: number): number;
+  }
+
+  const myDoubleFunction: DoubleValueFunc = (
+    value1: number,
+    value2: number
+  ): number => (value1 + value2) * 2;
+  console.log(myDoubleFunction(10, 20));
 }
