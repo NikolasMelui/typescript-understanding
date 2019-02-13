d252afa164ceaacfb0a18e88c0942daa83e03226
nikolasmelui <slak@samaradom.ru>
2019-02-13 09:40:39 +0400

    Add built-in generic example

--- generics/app.ts
+++ generics/app.ts
@@ -11,3 +11,8 @@ console.log(betterEcho<string>('Nikolay'));
 
 // console.log(betterEcho<string>(27)); // It will be an error here
 console.log(betterEcho(27)); // Automatical generic type definer
+
+// Built-in Generics
+const testResults: Array<number> = [1, 2, 3, 4, 5]; // Built-in Array Generic Type
+testResults.push(6, 7, 8, 9, 10); // We can push the typed data in the Generic typed array
+// testResults.push('hello'); // But can't doing it with another type
