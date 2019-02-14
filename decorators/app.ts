2045b9a1649e4b9d0f3b69e5b7898a1fb8e25659
nikolasmelui <slak@samaradom.ru>
2019-02-14 23:17:47 +0400

    Add simple factory to the simple decorator.

--- decorators/app.ts
+++ decorators/app.ts
@@ -1,10 +1,17 @@
 namespace Decorators {
-  function logged(constructor: Function) {
-    console.log(constructor);
-  }
+  // Add the simple decorator
+  const logged = (constructor: Function): void => console.log(constructor);
 
+  // Use the decorator
   @logged
   class Person {
+    constructor() {}
+  }
+
+  const logging = (value: boolean): any => (value ? logged : false);
+
+  @logging(true)
+  class Car {
     constructor() {}
   }
 }
